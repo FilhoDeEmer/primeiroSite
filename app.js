65a03f104efb040f2776c4d482828349a6fc5937
FilhoDeEmer <141789981+FilhoDeEmer@users.noreply.github.com>
2025-01-22 14:50:47 -0300

    Vinculações com base de dados via api, criação da tela de produtos, separaçoes de componentes para uso em diversas paginas

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ Parse.initialize(
 Parse.serverURL = "https://parseapi.back4app.com";
 
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
+app.use("/public", express.static("public"));
 
-//variavesi
+//variaveis
 allColections = [];
 
 // Rota principal que envia o arquivo index.html
@@ -27,12 +27,24 @@ app.get("/", async (req, res) => {
   }
 });
 
+//Rota para uma coleção especifica
+app.get("/colecao/:nomeColecao", async (req, res) => {
+  const nomeColecao = req.params.nomeColecao;
+  try {
+    const colecoes = await fetchColecoes();
+    const colecaoId = await fetchColecao(nomeColecao);
+    const allProducts = await fetchCarByColection(colecaoId);
+    res.render("colecao", { colecoes, allProducts }); // Passa as coleções diretamente para a view
+  } catch (error) {
+    res.status(500).send("Erro ao buscar coleções");
+    console.error("Erro ao renderizar a página principal", error);
+  }
+});
+
 // Função para buscar todos as coleções
 const fetchColecoes = async () => {
   const colecoes = Parse.Object.extend("colecao"); // aqui vai o nome da tabela
   const query = new Parse.Query(colecoes);
-
-  //query.where("email", "luis@gmail.com");
 
   try {
     const results = await query.find();
@@ -43,6 +55,40 @@ const fetchColecoes = async () => {
   }
 };
 fetchColecoes();
+
+//Função busca uma coleção especifica
+const fetchColecao = async (nomeColecao) => {
+  const colecoes = Parse.Object.extend("colecao"); // aqui vai o nome da tabela
+  const query = new Parse.Query(colecoes);
+  query.equalTo("nome", nomeColecao); // aqui vai o parametro de busca
+  try {
+    const results = await query.find();
+    if (results.length > 0) {
+      
+      return results[0].get("Id"); 
+    } else {
+      throw new Error(`Nenhuma coleção encontrada com o nome: ${nomeColecao}`);
+    }
+  } catch (error) {
+    console.error("Erro ao buscar coleções: ", nomeColecao, error);
+    throw error;
+  }
+};
+
+const fetchCarByColection = async (colecaoId) => {
+  const produtos = Parse.Object.extend("produtos"); // aqui vai o nome da tabela
+  const query = new Parse.Query(produtos);
+  query.equalTo("colecao", parseInt(colecaoId)); // aqui vai o parametro de busca
+  query.limit(250)
+
+  try {
+    const results = await query.find();
+    return results.map((produto) => produto.toJSON()); // Retorna como JSON
+  } catch (error) {
+    console.error("Erro ao buscar produto:", error);
+    throw error;
+  }
+};
 
 // Iniciar o servidor na porta especificada
 app.listen(port, () => {
